e894604bcdf64971ecda5470e6f66d741444ca69
yutee <yuteenightxi@gmail.com>
2024-05-30 01:45:49 +0100

    Azure SWA deployment with Github

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Yutee
+          This is Yutee
         </p>
         <a
           className="App-link"
